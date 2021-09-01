b17621b13c3d67ae3d63dd7b4b6d0503acf3665c
fede-veloper <fedev.code@gmail.com>
2021-09-01 07:11:21 -0500

    db

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const Note = require('./models/note');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-];
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method);
@@ -44,52 +27,78 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.json(notes);
+    Note.find({}).then(notes => res.json(notes));
 });
 
-app.get('/notes/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const note = notes.find(note => note.id === id);
-    if (note) {
-        res.json(note);
-    } else {
-        res.status(404).end();
-    }
+app.get('/notes/:id', (req, res, next) => {
+    Note.findById(req.params.id)
+        .then(note => {
+            if (note) {
+                res.json(note)
+            } else {
+                res.status(404).end();
+            }
+        }).catch(error => { next(error) });
 });
 
-app.post('/notes', (req, res) => { 
+app.post('/notes', (req, res, next) => { 
     const body = req.body;
 
-    if (!body.content) {
-        return res.status(400).json({error: 'content missing'});
-    }
-
-    const note = {
-        id: generateId(), 
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date()
-    }
+    });
 
-    notes = notes.concat(note);
-    res.send(note);
+    note.save()
+        .then(noteSaved => noteSaved.toJSON())
+        .then(savedAndFormattedNote => res.json(savedAndFormattedNote))
+        .catch(error => next(error));
 });
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
-    return maxId + 1;
-};
+app.delete('/notes/:id', (req, res, next) => {
+    Note.findByIdAndRemove(req.params.id)
+        .then(response => {
+            res.send(response);
+        })
+        .catch(error => {
+            next(error);
+        });
+});
 
-app.delete('/notes/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    notes = notes.filter((note) => {note.id !== id});
-    res.status(204).end();
+app.put('/notes/:id', (req, res, next) => {
+    const body = req.body;
+    const note = {
+        content: body.content, 
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(req.params.id, note, {new: true})
+        .then(result => {
+            res.send(result);
+        })
+        .catch(error => {
+            next(error);
+        });
 });
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 };
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message });
+    }
+
+    next(error);
+}
   
+app.use(errorHandler);  
 app.use(unknownEndpoint);
 
 app.listen(PORT, () => {
---
+++ models/note.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(ressult => {
+        console.log('connected to MongoDB');
+    })
+    .catch(error => {
+        console.log('error to connect ' + error.message);
+    });
+
+const noteSchema = mongoose.Schema({
+    content: {
+        type: String,
+        minlength: 5,
+        require: true
+    }, 
+    date: {
+        type: Date,
+        require: true
+    }, 
+    important: Boolean
+});
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+});
+
+module.exports = mongoose.model('Note', noteSchema);
