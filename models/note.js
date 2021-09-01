84edd8f691341e213a84fa8805e43c38e266aa1b
fede-veloper <fedev.code@gmail.com>
2021-09-01 11:21:24 -0500

    lint

--- models/note.js
+++ models/note.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(ressult => {
-        console.log('connected to MongoDB');
-    })
-    .catch(error => {
-        console.log('error to connect ' + error.message);
-    });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error to connect ' + error.message)
+  })
 
 const noteSchema = mongoose.Schema({
-    content: {
-        type: String,
-        minlength: 5,
-        require: true
-    }, 
-    date: {
-        type: Date,
-        require: true
-    }, 
-    important: Boolean
-});
+  content: {
+    type: String,
+    minlength: 5,
+    require: true
+  },
+  date: {
+    type: Date,
+    require: true
+  },
+  important: Boolean
+})
 
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-});
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
